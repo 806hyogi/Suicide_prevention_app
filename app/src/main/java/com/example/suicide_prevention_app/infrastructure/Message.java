1d9ccf5e64f1d32d72e6fcecbea793e59c3e0d65
Namani31 <kwg531@naver.com>
2023-06-09 23:18:41 +0900

    FEAT: 사진 처리를 위한 getImageUrl() 추가
    Message.java에 사진 처리를 위한 getImageUrl() 추가

--- app/src/main/java/com/example/suicide_prevention_app/infrastructure/Message.java
+++ app/src/main/java/com/example/suicide_prevention_app/infrastructure/Message.java
@@ -5,10 +5,12 @@ package com.example.suicide_prevention_app.infrastructure;
 public class Message {
     private String content; // 메시지 내용 저장
     private boolean isUserMessage; // 사용자 메시지 여부
+    private String imageUrl; // 이미지 URL
 
-    public Message(String content, boolean isUserMessage) {
+    public Message(String content, boolean isUserMessage, String imageUrl) {
         this.content = content;
         this.isUserMessage = isUserMessage;
+        this.imageUrl = imageUrl;
     }
 
     /* 메시지 내용 반환 */
@@ -19,5 +21,9 @@ public class Message {
     /* 사용자 메시지 여부 반환 */
     public boolean isUserMessage() {
         return isUserMessage;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
     }
 }
